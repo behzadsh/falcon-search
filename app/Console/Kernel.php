8317f8f995effb3c667918139ce297bd4b99db7d
Behzad Shabani <behzad.shabani@gmail.com>
2015-11-13 23:47:15 +0330

    Fix renamed command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \FalconSearch\Console\Commands\Crawl\GetNodes::class,
+        \FalconSearch\Console\Commands\Crawl\QueueNodes::class,
     ];
 
     /**
