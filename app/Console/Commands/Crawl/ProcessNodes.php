452f04e3bb97c24804c44fceca7e5a2eff8db63d
Behzad Shabani <behzad.shabani@gmail.com>
2015-11-20 10:01:17 +0330

    Update ProcessNodes command

--- app/Console/Commands/Crawl/ProcessNodes.php
+++ app/Console/Commands/Crawl/ProcessNodes.php
@@ -4,9 +4,11 @@ namespace FalconSearch\Console\Commands\Crawl;
 
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Redis\Database;
+use PHPHtmlParser\Dom;
 
 class ProcessNodes extends Command
 {
+
     /**
      * The name and signature of the console command.
      *
@@ -21,20 +23,44 @@ class ProcessNodes extends Command
      */
     protected $description = 'Crawl nodes data and index them';
 
+    protected $ignoringTags = [
+        'head',
+        'style',
+        'script',
+        'noscript',
+        'iframe',
+        'form',
+        'header',
+        'nav',
+        'navbar',
+        'aside',
+        'button',
+        'meta',
+        'link',
+        'textarea'
+    ];
+
     /**
      * @var Database
      */
     protected $redis;
 
     /**
+     * @var Dom
+     */
+    protected $dom;
+
+    /**
      * Create a new command instance.
      *
      * @param Database $redis
+     * @param Dom      $dom
      */
-    public function __construct(Database $redis)
+    public function __construct(Database $redis, Dom $dom)
     {
         parent::__construct();
         $this->redis = $redis;
+        $this->dom = $dom;
     }
 
     /**
@@ -44,6 +70,39 @@ class ProcessNodes extends Command
      */
     public function handle()
     {
+        $this->redis->subscribe(['nodes-channel'], function ($url) {
+            $cachePage = storage_path('caches/' . md5($url) . '.html');
+            if (!file_exists($cachePage)) {
+                file_put_contents($cachePage, file_get_contents($url));
+            }
+            $this->dom->load($cachePage);
+            $this->info($this->stripTags($this->cleanContent($this->dom->root)));
+        });
+    }
+
+    protected function cleanContent(Dom\AbstractNode $node)
+    {
+        if ($node->hasChildren()) {
+            foreach ($node->getChildren() as &$child) {
+                if ($this->shoulIgnore($child->getTag()->name())) {
+                    $node = $node->removeChild($child->id());
+                } else {
+                    $child = $this->cleanContent($child);
+                }
+            }
+        }
+
+        return $node;
+    }
+
+    protected function shoulIgnore($name)
+    {
+        return in_array($name, $this->ignoringTags);
+    }
+
+    protected function stripTags($content)
+    {
+        return trim(preg_replace('/\s+/', ' ', preg_replace('/<[^>]+>/', ' ', $content)));
     }
 
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \FalconSearch\Console\Commands\Crawl\QueueNodes::class,
+        \FalconSearch\Console\Commands\Crawl\ProcessNodes::class,
     ];
 
     /**
