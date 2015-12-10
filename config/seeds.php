6ec0bdd310076a3be90ee8d91b8e5f99fd33e9c9
Behzad Shabani <behzad.shabani@gmail.com>
2015-12-11 02:26:18 +0330

    Update seeds

--- config/seeds.php
+++ config/seeds.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
-    'http://edition.cnn.com/robots.txt'
+    'http://edition.cnn.com/robots.txt',
+    'http://www.bbc.co.uk/robots.txt',
+    'http://www.reuters.com/robots.txt',
+    'http://www.ap.org/robots.txt',
+    'http://www.upi.com/robots.txt',
+    'http://www.foxnews.com/robots.txt',
+    'http://www.euronews.com/robots.txt',
+
+    'http://www.ilna.ir/robots.txt',
+    'http://www.farsnews.com/robots.txt',
+    'http://www.mehrnews.com/robots.txt',
+    'http://www.tasnimnews.com/robots.txt',
 ];
